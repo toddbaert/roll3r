985c62f6eb4b2a1fb89b3a88d4af9dc697c9f3c0
todd <tbaert@redhat.com>
2018-03-18 21:00:21 -0400

    rollin

--- simple-roller.js
+++ simple-roller.js
@@ -2,7 +2,14 @@
 
     const dieSvgs = document.getElementsByClassName('die');
 
-    Array.prototype.forEach.call(dieSvgs, function(a){
-        
+    Array.prototype.forEach.call(dieSvgs, function(elem){
+        elem.onclick = function(e, x) {
+            var that = this;
+            that.classList.add('roll');        
+            setTimeout(function(){
+                that.classList.remove('roll');
+            }, 1000);
+        };
     });
+    
 })();
