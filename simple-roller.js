4ca5af006417842ded2919e53f9121ca21b81247
todd <tbaert@redhat.com>
2018-03-24 19:09:41 -0400

    starting on generating scores

--- simple-roller.js
+++ simple-roller.js
@@ -1,15 +1,15 @@
 (function(){
-
-    const dieSvgs = document.getElementsByClassName('die');
-
+    const dieSvgs = document.getElementsByClassName('die-container');
     Array.prototype.forEach.call(dieSvgs, function(elem){
-        elem.onclick = function(e, x) {
+        elem.onclick = function(e) {
             var that = this;
-            that.classList.add('roll');        
+            that.classList.add('roll');
+            setTimeout(function(){
+                //TODO: change data-score attr
+            }, 500);   
             setTimeout(function(){
                 that.classList.remove('roll');
             }, 1000);
         };
     });
-    
 })();
