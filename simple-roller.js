86f957d287d6098958a2fcdb5af929ff53ea977a
todd <tbaert@redhat.com>
2018-03-31 21:00:13 -0400

    implementing scoring

--- simple-roller.js
+++ simple-roller.js
@@ -1,14 +1,19 @@
 (function(){
+    const ROLL = 'roll';
+    const DATA_MAX = 'data-max';
+    const DATA_SCORE = 'data-score';
     const dieSvgs = document.getElementsByClassName('die-container');
+    
     Array.prototype.forEach.call(dieSvgs, function(elem){
-        elem.onclick = function(e) {
+        elem.onclick = function(e, t) {
             var that = this;
-            that.classList.add('roll');
+            that.classList.add(ROLL);
             setTimeout(function(){
-                //TODO: change data-score attr
-            }, 500);   
+                var max = that.getAttribute(DATA_MAX);
+                that.setAttribute(DATA_SCORE, Math.ceil(Math.random() * max));
+            }, 900);   
             setTimeout(function(){
-                that.classList.remove('roll');
+                that.classList.remove(ROLL);
             }, 1000);
         };
     });
